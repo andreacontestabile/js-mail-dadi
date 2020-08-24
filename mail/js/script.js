7498f3673ef6ec122aa72c191ba8e4134483a745
Andrea Contestabile <andreacontest@gmail.com>
2020-08-24 16:11:04 +0200

    Controllo mail JS

--- mail/js/script.js
+++ mail/js/script.js
@@ -3,10 +3,16 @@ var userEmail = prompt("Inserisci il tuo indirizzo email");
 // 2. Controllo che l'indirizzo sia nella lista di quelli autorizzati
   // 2.1. Creo un array contenente gli indirizzi autorizzati
 var listaEmail = ["andrea@gmail.com", "luca@gmail.com", "paola@gmail.com", "sara@gmail.com", "gianni@gmail.com", "valeria@gmail.com", "claudio@gmail.com"];
-  // 2.2. Attraverso l'array per verificare se l'indirizzo dell'utente è presente nella lista
+  // 2.2. Tengo traccia di una variabile per verificare le presenza dell'indirizzo nell'array
+var emailAutorizzata = false;
 for (var i = 0; i < listaEmail.length; i++) {
   if (userEmail == listaEmail[i]) {
 // 3. Stampo un messaggio in base all'esito del controllo
+    emailAutorizzata = 1;
     console.log("L'indirizzo è autorizzato ad accedere. Benvenuto!")
   }
+}
+
+if (emailAutorizzata == false) {
+  console.log("Purtroppo l'indirizzo non è autorizzato. Riprova con un altro indirizzo.");
 }
